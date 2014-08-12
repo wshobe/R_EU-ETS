134a21fd8c8a635ed983494eefe5686db26c3f8e
Bill Shobe <wms5f@d-128-49-89.bootp.virginia.edu>
2014-08-12 13:30:47 -0400

    Read session data from csv file. Loop over sessions in CSV file. Write data to database.

--- Code/ETSsim.R
+++ Code/ETSsim.R
@@ -1,61 +1,33 @@
 #con1 <- dbConnect(MySQL(),user="wms5f", password="manx(0)Rose",dbname="Experiments", host="econ.ccps.virginia.edu")
 #script  <- paste("show tables")
 #test = dbGetQuery(con1, script)
-
-set.seed(1234)
+require(taRifx)    # For 'shift' function
 rm(list = ls(all = TRUE)) 
-i = 1
+#i = 1
 # set up session variables
 sessionData = fread("simValues.csv")
-session = sessionData[1]
-# session = data.table(id=i)
-# session$name = "Myopic agents,liquidity collar,reserve price"
-# session$numValues = 20
-# session$numHighUsers = 3
-# session$numLowUsers = 3
-# session$highUserCapacity = 4
-# session$lowUserCapacity = 4
-# session$highUserIntensity = 2
-# session$lowUserIntensity = 1
-# session$highUserInitBank = 1
-# session$lowUserInitBank = 2
-# session$lowUserInitCapital = 50
-# session$highUserInitCapital = 100
-# session$numPeriods = 18
-# session$initialReserve = 12
-# session$initialCapAmount = 29
-# session$capReduction = 1
-# session$reservePrice = 7
-# session$priceCap = F
-# session$highPriceLimit = ifelse(session$priceCap, 15, NA)
-# session$baseOutputPrice = 30
-# session$lowUserCostMin = 10
-# session$lowUserCostMax = 30
-# session$highUserCostMin = 0
-# session$highUserCostMax = 30
-# session$highOutputPriceProbability = 0.5
-# session$liquidityCollar = T
-# if (session$liquidityCollar) {
-#   session$triggerQuantityLowerBound = 18
-#   session$triggerQuantityUpperBound = 40
-#   session$triggerQuantityInjection = 6
-#   session$triggerQuantityReductionProportion = 0.15
-# }
+numSessions = nrow(sessionData)
+# Main session loop
+for (sessionNum in sessionData$id) {
+#sessionNum=1
+#
+session = sessionData[sessionNum]
+set.seed(1234)
 session$numSubjects = session$numLowUsers + session$numHighUsers
 attach(session)
   session$initialBank = lowUserInitBank*numLowUsers + highUserInitBank*numHighUsers
   session$fullCapacity = numLowUsers*lowUserCapacity*lowUserIntensity + numHighUsers*highUserCapacity*highUserIntensity
   session$sessionDataLength = numPeriods*numSubjects*numValues
   #
-  period = data.table(id=1:numPeriods)
+  period = data.table(periodNum=1:numPeriods,key="periodNum")
   period$outputPrice = rep(baseOutputPrice,numPeriods) + rbinom(numPeriods,1,highOutputPriceProbability)*10
   period$cap = seq(initialCapAmount,by=-capReduction,length.out=numPeriods)
-  period$sessionID = id
+  period$sessionID = sessionNum
 detach(session)
 period[,`:=`(
               auctionPrice = 0, auctionQuantitySold=0
               )]
-period[id==1,`:=`(
+period[periodNum==1,`:=`(
               startingTotalBank=session$initialBank,
               startingReserve = session$initialReserve
               )]
@@ -64,9 +36,10 @@ session$numHighPricePeriods = period[outputPrice>session$baseOutputPrice,length(
 session$totalCap = period[,sum(cap)]
 attach(session)
   subjectIDs = rep(1:numSubjects, times=numPeriods)
-  item = data.table(id=array(1:sessionDataLength))
+  item = data.table(itemID=array(1:sessionDataLength),key="itemID")
   item$subjectID = rep(subjectIDs, each=numValues)
-  item$periodID = rep(period$id,each=numSubjects*numValues)
+  item$periodNum = rep(period$periodNum,each=numSubjects*numValues)
+  item$sessionID = sessionNum
   lowUnits = rep(c(rep(1,lowUserCapacity*lowUserIntensity),rep(0,numValues-lowUserCapacity*lowUserIntensity)),times=numLowUsers)
   highUnits = rep(c(rep(1,highUserCapacity*highUserIntensity),rep(0,numValues-highUserCapacity*highUserIntensity)),times=numHighUsers)
   item$unit = rep(c(lowUnits,highUnits),times=numPeriods)  #for high users each entry is a half capacity unit
@@ -76,9 +49,9 @@ attach(session)
   item[unitIntensity==highUserIntensity,rndCostFactor:=rep(runif(numValues*numHighUsers*numPeriods),each=2,)]
 # Costs are for production units only, set other values in 'for t' loop
   item[,cost:=round(unit*ifelse(unitIntensity==lowUserIntensity,(rndCostFactor*20+10),unit*(rndCostFactor*30)/unitIntensity),digits=2)]
-  setkey(item,periodID);setkey(period,id)
+  setkey(item,periodNum);setkey(period,periodNum)
   item[period,value:=unit*(outputPrice/unitIntensity-cost)]
-  setkey(item,periodID,subjectID,value)
+  setkey(item,periodNum,subjectID,value)
 # Assign unit numbers from high to low, since sorting of values is from low to high. So unit 1 has the highest value, etc.
   item[unit==1 & unitIntensity==lowUserIntensity,unitNum:=rep(lowUserCapacity:1,times=(numLowUsers*numPeriods))]
   item[unit==1 & unitIntensity==highUserIntensity,unitNum:=rep(highUserCapacity:1,each=2,times=(numHighUsers*numPeriods))]
@@ -86,11 +59,11 @@ attach(session)
 # Set up subject data table [numSubjects*numPeriods,]
   subject = data.table(id=1:(numSubjects*numPeriods))
   subject[,`:=`(subjectID=subjectIDs,
-                periodID=rep(1:numPeriods,each=numSubjects),
+                periodNum=rep(1:numPeriods,each=numSubjects),
                 startingBank=0, startingCash=0, earnings=0, endingCash=0,
                 endingBank = 0, startingDeficit=0,endingDeficit=0,permitsUsed=0
           )]
-  subject[periodID==1,`:=`(startingBank=c(rep(lowUserInitBank,times=numLowUsers),rep(highUserInitBank,times=numHighUsers)),
+  subject[periodNum==1,`:=`(startingBank=c(rep(lowUserInitBank,times=numLowUsers),rep(highUserInitBank,times=numHighUsers)),
                            startingCash=c(rep(lowUserInitCapital,times=numLowUsers),rep(highUserInitCapital,times=numHighUsers))
                            )]
 #
@@ -103,19 +76,17 @@ attach(session)
 detach(session)
 #
 #periods = sort(as.array(period[,.SD[,id]]))
-#  t.bids = item[,list(id,value,unit,periodID)][order(-value)]
+#  t.bids = item[,list(id,value,unit,periodNum)][order(-value)]
 
-for (t in period$id) {     #period$id
+for (t in period$periodNum) {     #period$periodNum
 #t = 1
-  setkey(item,periodID)
+  setkey(item,periodNum)
   thisPeriod = period[t]
   t.cap = thisPeriod$cap
   if (t==1) {
-    print(t)
     currentReserve = session$initialReserve
     t.bank = thisPeriod$startingTotalBank
   } else {
-    print(t)
     currentReserve = period$endingReserve[t-1]
     t.bank = period$endingTotalBank[t-1]
     period[t]$startingTotalBank = t.bank
@@ -134,21 +105,20 @@ for (t in period$id) {     #period$id
       reserveAdjustment = -1*floor(period$endingTotalBank[t-2]*session$triggerQuantityReductionProportion)
     } 
   }
-  cat(sprintf("ReserveAdjustment: %d\n", reserveAdjustment))
+#  cat(sprintf("ReserveAdjustment: %d\n", reserveAdjustment))
 #  print(reserveAdjustment)
   currentReserve = max(0,currentReserve - reserveAdjustment)
-cat(sprintf("currentReserve: %d, current bank: %d\n", currentReserve,t.bank))
+#cat(sprintf("currentReserve: %d, current bank: %d\n", currentReserve,t.bank))
 auctionQuantity = t.cap +  reserveAdjustment
 
 #  bankDemandShift = ifelse(t.bank>session$fullCapacity,session$fullCapacity,t.bank)   # in Myopic sessions, use bank ASAP
 bankDemandShift = min(t.bank,session$fullCapacity-session$numSubjects)
-cat(sprintf("bankDemandShift: %d\n", bankDemandShift))
+#cat(sprintf("bankDemandShift: %d\n", bankDemandShift))
 
-t.bids = item[.(t),list(id,value,unit)]
+  t.bids = item[.(t),list(itemID,value,unit)]
   t.bids = t.bids[order(-value)]
 #  minBidValue = session$dynamicEfficientPrice # Perfect foresight
   minBidValue = 0 # Myopic bids
-require(taRifx)    # For 'shift' function
 # Allowances in the bank come off the top of allowance values
 # Later, we can do this by bidder, but for now it is annonymous
 #print(t.bids[,value])
@@ -164,10 +134,10 @@ require(taRifx)    # For 'shift' function
   t.bids[,bid:=ifelse(bid>=thisReservePrice,bid,NA)]  # No bids less than the reserve price are allowed. NA is no bid
 
 #  currentPermitValue = ifelse(is.na(t.bids$value[auctionQuantity + t.bank ]),0,t.bids$value[auctionQuantity])
-#  setkey(t.bids,periodID)
+#  setkey(t.bids,periodNum)
 # Auction reconciliation
 # This is where the upper price collar function goes
-cat(sprintf("auctionQuantity: %d\n", auctionQuantity))
+#cat(sprintf("auctionQuantity: %d\n", auctionQuantity))
 auctionPrice = t.bids$bid[auctionQuantity + 1 ]
 if (session$priceCap) {
   if(!is.na(auctionPrice) & auctionPrice > session$highPriceLimit ) {
@@ -181,7 +151,7 @@ if (session$priceCap) {
   }
 }
 #
-  setkey(t.bids,id)
+  setkey(t.bids,itemID)
   t.bids$bidAccepted=0
   if (is.na(auctionPrice)) {
     auctionPrice = thisReservePrice
@@ -189,7 +159,7 @@ if (session$priceCap) {
     auctionExcessSupply=auctionQuantity - numPurchased
 # Unsold permits go into the reserve
     currentReserve = currentReserve + auctionExcessSupply
-    cat(sprintf("NA price: auctionExcessSupply: %d\n", auctionExcessSupply))
+#    cat(sprintf("NA price: auctionExcessSupply: %d\n", auctionExcessSupply))
 # t.bids is sorted by value descending
     t.bids[1:numPurchased,bidAccepted:=1]
   } else {
@@ -199,14 +169,14 @@ if (session$priceCap) {
     if (tieLength>1 & t.bids$value[auctionQuantity ] == auctionPrice) {
       t.bids[,bidAccepted:=ifelse(bid>auctionPrice,1,0)]
       numNeeded = auctionQuantity - length(t.bids[bid>auctionPrice])
-      idArray = data.table(id=sample(t.bids[tieKey==1,id],numNeeded,replace=F),key="id")
+      idArray = data.table(id=sample(t.bids[tieKey==1,itemID],numNeeded,replace=F),key="id")
       t.bids[idArray,bidAccepted:=1]
     } else {
       t.bids[,bidAccepted:=ifelse(bid>=auctionPrice,1,0)]
     }
     numPurchased = auctionQuantity 
   }
-cat(sprintf("auctionPrice: %5.2f,  numPurchased: %d\n", auctionPrice,numPurchased))
+#cat(sprintf("auctionPrice: %5.2f,  numPurchased: %d\n", auctionPrice,numPurchased))
 # Production decision
   t.bids = t.bids[order(-value)]
   permitsAvailable = numPurchased + t.bank
@@ -216,12 +186,12 @@ cat(sprintf("auctionPrice: %5.2f,  numPurchased: %d\n", auctionPrice,numPurchase
   thisSurplus = t.bids[,sum(unitUsed*value)]
   numPermitsUsed = t.bids[,sum(unitUsed)]
   t.bank = max(0,permitsAvailable - numPermitsUsed)
-cat(sprintf("Permits available: %d,   Permits used: %d\n", permitsAvailable,numPermitsUsed))
-cat(sprintf("t.bank: %d \n", t.bank))
+#cat(sprintf("Permits available: %d,   Permits used: %d\n", permitsAvailable,numPermitsUsed))
+#cat(sprintf("t.bank: %d \n", t.bank))
 #
-#  t.bids[,`:=`(unit=NULL,value=NULL,periodID=NULL)]
-  setkey(item,id)
-  setkey(t.bids,id) #<- this is already done above
+#  t.bids[,`:=`(unit=NULL,value=NULL,periodNum=NULL)]
+  setkey(item,itemID)
+  setkey(t.bids,itemID) #<- this is already done above
   item[t.bids,`:=`(bidAccepted=i.bidAccepted,unitUsed=i.unitUsed)]
   price = auctionPrice
   period[t,`:=`(auctionPrice=price,
@@ -232,6 +202,12 @@ cat(sprintf("t.bank: %d \n", t.bank))
                 permitsUsed = numPermitsUsed
                 )]
 }
-production = item[unitUsed==1,list(production=sum(ifelse(unitIntensity==1,1.0,0.5))),by=periodID]
-dynamicProduction = item[dynamicProducingUnit==1,list(dynamicProduction=sum(ifelse(unitIntensity==1,1.0,0.5))),by=periodID]
-session$staticSurplus = item[unitUsed==1,sum(value-session$dynamicEfficientPrice)]
+con1 <- dbConnect(MySQL(),user="wms5f", password="manx(0)Rose",dbname="test", host="localhost")
+dbWriteTable(con1,"period",period,append=T,row.names=F)
+dbWriteTable(con1,"session",session,append=T,row.names=F)
+dbWriteTable(con1,"item",item,append=T,row.names=F)
+dbDisconnect(con1)
+}
+# production = item[unitUsed==1,list(production=sum(ifelse(unitIntensity==1,1.0,0.5))),by=periodNum]
+# dynamicProduction = item[dynamicProducingUnit==1,list(dynamicProduction=sum(ifelse(unitIntensity==1,1.0,0.5))),by=periodNum]
+# session$staticSurplus = item[unitUsed==1,sum(value-session$dynamicEfficientPrice)]
